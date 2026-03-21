7d409e9894141bf9f66fce8ea7f32d5443d713ab
kuba <jakubszrajber12@gmail.com>
2026-03-21 22:33:27 +0100

    adding followers count for drf user info display

--- backend/apps/api/serializers.py
+++ backend/apps/api/serializers.py
@@ -4,9 +4,18 @@ from backend.apps.posts.models import Post
 
 
 class UserSerializer(serializers.ModelSerializer):
+    followers_count = serializers.SerializerMethodField()   # DRF ignores "get_" and sends the value to proper variable
+    following_count = serializers.SerializerMethodField()
+
+    def get_followers_count(self, obj):
+        return obj.followers.count()
+
+    def get_following_count(self, obj):
+        return obj.following.count()
+
     class Meta:
         model = User
-        fields = ["id", "username", "last_login", "email", "date_joined"]
+        fields = ["id", "username", "last_login", "email", "followers_count", "following_count", "date_joined"]
 
 
 class PostSerializer(serializers.ModelSerializer):
